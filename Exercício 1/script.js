1ead298366376cd91bb2b3fb33af635515f2018c
carsvz <electronapifra@gmail.com>
2025-05-21 22:31:08 -0300

    Update script.js

--- Exercício 1/script.js
+++ Exercício 1/script.js
@@ -1,3 +1,14 @@
-console.log("ai carai")
-window.prompt("ai")
-alert("ai ")
+num1 = prompt("digite o primeiro numero: ");
+num2 = prompt("digite o segundo numero: ");
+
+
+
+soma = num1 + num2;
+
+
+
+if(soma % 2 == 0){
+    alert("par");
+}else{
+    alert("impar")
+}
